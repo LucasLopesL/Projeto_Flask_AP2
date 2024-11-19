2f2f584e76e7c7663975ed5b9a845b3d75df37f1
LUCAS LOPES <lucaslopesfnance@gmail.com>
2024-11-19 16:05:23 -0300

    update main

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return jsonify({"message": "Hello, World!"})
+    return jsonify({"message": "Lucas aqui, professor! Suas aulas foram muito boas."})
 
 if __name__ == "__main__":
     app.run(debug=True)
