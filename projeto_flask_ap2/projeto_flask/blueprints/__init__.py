37dfea71bc141a9fe3dd2f8eca4e4178d168f0a6
LUCAS LOPES <lucaslopesfnance@gmail.com>
2024-11-19 16:00:29 -0300

    código

--- projeto_flask_ap2/projeto_flask/blueprints/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-
-crud_bp = Blueprint('crud', __name__)
-professor_bp = Blueprint('professor', __name__)
-aluno_bp = Blueprint('aluno', __name__)
-turma_bp = Blueprint('turma', __name__)
--- projeto_flask_ap2/projeto_flask/blueprints/professor.py
+++
@@ -1,9 +0,0 @@
-from flask import render_template
-from projeto_flask import database
-from projeto_flask.models.models import Professor
-from blueprints import professor_bp
-
-@professor_bp.route("/read/professor", methods=["GET", "POST"])
-def professor():
-    lista_professor = database.session.query(Professor).all()
-    return render_template("professor.html", lista_professor=lista_professor)
